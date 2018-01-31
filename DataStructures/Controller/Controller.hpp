96669e5c35513466773b580d51c0f63388e2d9be
Dustin Schimel <dustin.schimel@gmail.com>
2018-01-31 11:41:06 -0700

    Created the runner and controller files

---
+++ DataStructures/Controller/Controller.hpp
@@ -0,0 +1,22 @@
+//
+//  Controller.hpp
+//  DataStructures
+//
+//  Created by Schimel, Dustin on 1/31/18.
+//  Copyright © 2018 Schimel, Dustin. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <iostream>
+#include <vector>
+
+class Controller
+{
+public:
+    void start();
+    
+};
+#endif /* Controller */
+
