79b949bc3adbb6c889aefc16258524c45d0b4504
Dustin Schimel <dustin.schimel@gmail.com>
2018-02-02 13:53:33 -0700

    Added uses for the newly added array template

--- DataStructures/Controller/Controller.cpp
+++ DataStructures/Controller/Controller.cpp
@@ -65,5 +65,12 @@ void Controller :: findMaxAndMin()
 
 void Controller :: testArray()
 {
+    Array<int>dustinArray(45);
     
+    cout << dustinArray.getSize() << endl;
+    
+    dustinArray[0] = 123;
+    dustinArray.setAtIndex(0, 321);
+    
+    cout << dustinArray[0] << endl;
 }
