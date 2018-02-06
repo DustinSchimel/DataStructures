945846843935005c07dfeb9e5393f478b21310ea
Dustin Schimel <dustin.schimel@gmail.com>
2018-02-06 12:06:52 -0700

    Created the ArrayTester cpp and hpp files

---
+++ DataStructures/Testers/ArrayTester.cpp
@@ -0,0 +1,9 @@
+//
+//  ArrayTester.cpp
+//  DataStructures
+//
+//  Created by Schimel, Dustin on 2/6/18.
+//  Copyright © 2018 Schimel, Dustin. All rights reserved.
+//
+
+#include "ArrayTester.hpp"
