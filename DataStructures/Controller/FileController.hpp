2b6cc414a702ab9dbd8e39a60056bd21bae1f8ce
Dustin Schimel <dustin.schimel@gmail.com>
2018-02-14 13:47:21 -0700

    Created LinkedListTester

--- DataStructures/Controller/FileController.hpp
+++ DataStructures/Controller/FileController.hpp
@@ -15,6 +15,7 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#import "../Model/Structures/LinkedList.hpp"
 
 using namespace std;
 
@@ -23,5 +24,46 @@ class FileController
 public:
     static vector<CrimeData> readCrimeDataToVector(string filename);
 };
+
+static LinkedList<CrimeData> readDataToList(string filename);
+
+LinkedList<CrimeData> FileController :: readDataToList(stirng fileName)
+{
+    LinkedList<CrimeData> crimes;
+    
+    string curretnCSVLine;
+    int rowCount = 0;
+    
+    ifstream dataFile(fileName);
+    
+    //If the file exists at that path.
+    if (dataFile.is_open())
+    {
+        //Keep reading until you are at the end of the file.
+        while (!datFile.eof())
+        {
+            //Grab each  line from the CSV seperated by the carriage return character.
+            getline(dataFile, currentCSVLine, '\r');
+            //Exclude header row
+            if (rowCount != 0)
+            {
+                //Create a CrimeData instance from the line. Exclude a black line (usually if opened seperately)
+                if(currentCSVLine.length() != 0)
+                {
+                    CrimeData row(currentCSVLine);
+                    crimes.add(row);
+                }
+            }
+            rowCount++;
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "NO FILE" << endl;
+    }
+    
+    return crimes;
+}
 
 #endif /* FileController_hpp */
---
+++ DataStructures/Testers/LinkedListTester.cpp
@@ -0,0 +1,9 @@
+//
+//  LinkedListTester.cpp
+//  DataStructures
+//
+//  Created by Schimel, Dustin on 2/14/18.
+//  Copyright © 2018 Schimel, Dustin. All rights reserved.
+//
+
+#include "LinkedListTester.hpp"
